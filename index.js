6eacdfd74efa9df3d4738cdce93c43356db033ed
SirKawaii <mitchell@hotmail.cl>
2020-06-17 00:50:26 -0400

    now with api reader.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Utils = require('./utils/utils.js')
 const Discord = require('discord.js');
 const googleTTS = require('google-tts-api');
+const { default: utils } = require('./utils/utils.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -26,6 +27,15 @@ client.on('message', msg => {
   if(msg.content === prefix + 'leave'){
     talker.leave(msg);
   }
+  if(msg.content === prefix + 'get'){
+    talker.GetFromAPi(msg);
+  }
+  if(msg.content.startsWith(prefix + 'set_token')){
+    talker.SetAccessToken(msg);
+  }
+  if(msg.content.startsWith(prefix + 'set_video')){
+    talker.SetLiveVideoID(msg);
+  }
 });
 
 // client login .
@@ -36,20 +46,19 @@ client.login(token);
 
 const talker = {
 
-  current_channel: "",
+  current_channel: null,
+  facebook_access_token: null,
+  live_channel_id: null,
 
   test(message){
     //message.reply('que wea?');
     //console.log(message.author.username);
-
     let test = message;
-
     console.log(JSON.stringify(test));
-
   },
 
   help(message){
-    //noptingh
+    // work in progress
   },
 
   join(message){
@@ -64,19 +73,8 @@ const talker = {
   },
 
   leave(message){
-    console.log("leave:");
-    if(this.current_channel !== ""){
-      message.reply("y se marcho.");
-      
-      if (message.guild.me.voiceChannel !== undefined) {
-        message.guild.me.voiceChannel.leave();
-        message.reply("I have successfully left the voice channel!");
-      } else {
-        message.reply("I'm not connected to a voice channel!");
-      }
-    }else{
-      message.reply("No estoy en un canal de voz pao culiao.")
-    }
+    //nothing yet
+    
   },
 
   talk(message){
@@ -94,10 +92,10 @@ const talker = {
         if (channel && channel.type === 'voice'){
           channel.join().then((conn) => {
             // que tiene esta wea
-            let msg = current_message === null? "que wea querí": current_message;
+            let msg = current_message === null? "que weá querí!": current_message;
             googleTTS(msg
             ,'es_ES',
-            0.8)
+            1) // velocitiiiiii 0.4 to 1
             .then(function (url){
               conn.play(url);
             })
@@ -107,7 +105,82 @@ const talker = {
     }else{
       message.reply('Acuerdate unirte a un canal de voz pao culiao!');
     }
+  },
+
+  GetFromAPi(message){
+
+    Utils.DoRequest(Utils.options, function(err, result){
+      if(err){
+          return console.log("Error while trying to get the data ", err);
+      }
+      Object.keys(result).forEach(el =>{
+
+        let current = result[el]
+
+        if(current.id === 7){
+          message.reply(current.name);
+        }
+      });
+    });
+  },
+
+  GetLiveVideoChat(message){
+
+    if(this.live_channel_id === null){
+      message.replace("Acuerdate de setear el channel id.");
+      return;
+    }
+    if( this.facebook_access_token === null){
+      message.replace("Acuerdate de setear el access token con !SET.");
+      return;
+    }
+
+    let lastMessage = null;
+
+    let apiPath = "/" + this.live_channel_id + "/live_comments?access_token=" + this.facebook_access_token
+
+    let options = {
+      host: "https://graph.facebook.com",
+      port: 80,
+      path: apiPath,
+      method: 'GET'
+    }
+
+    Utils.DoRequest(options, function(err, result){
+      if(err){
+          return console.log("Error while trying to get the data ", err);
+      }
+
+      Object.keys(result.data).forEach(el =>{
+
+        let current = result[el]
+        talker.talk(current.message);
+      });
+    });
+  },
+
+  SetLiveVideoID(message){
+    console.log("Set video id process...");
+    let id = message.content.replace(prefix + "set_video","").replace(/\s/g, "");
+    if(id !== ""){
+      this.live_channel_id = id;
+      console.log("video id has saved:"  + id)
+      message.reply("Live video ID has been saved.");
+    }else{
+      message.reply("something happends. 🦽");
+    }
+    console.log("Set video id ended");
+  },
+
+  SetAccessToken(message){
+    let token = message.content.replace(prefix + "set_token","").replace(/\s/g, "");
+    if(token !== ""){
+      this.facebook_access_token = token;
+      console.log("token saved :" + token)
+      message.reply("Token has been saved.");
+    }else{
+      message.reply("something happends. 🦽");
+    }
   }
+
 }
-
-
--- utils/utils.js
+++ utils/utils.js
@@ -1,11 +1,70 @@
 // utils.js
 const fs = require('fs');
+const http = require('http');
+const { resolveTxt } = require('dns');
 
 function GetToken() {
     let secretFile = require('../utils/secret.json');
     return secretFile.discordApi;
 }
 
+function GetFacebookToken(){
+    //not inplemented
+}
+
+// json query option.
+let options = {
+    host: "cryptic-woodland-18269.herokuapp.com",
+    port: 80,
+    path: "/api/pokemons",
+    method: 'GET'
+};
+
+function DoRequest(options, cb){
+    http.request(options, function(res){
+        let body = '';
+        res.on('data', function(chunk){
+            body+= chunk;
+        });
+
+        res.on('end', function(){
+            let result = JSON.parse(body);
+            cb(null, result);
+        });
+        res.on('error', cb);
+    })
+    .on('error', cb)
+    .end();
+}
+
+function GetApiRequest(){
+    let thing = null;
+
+    DoRequest(options, function(err, result){
+        if(err){
+            return console.log("Error while trying to get the data ", err);
+        }
+        thing = result;
+    });
+    return thing;
+}
+
+function ListenChatStream(options, msg){
+
+    http.get({
+        agent: false,
+        path: options.path,
+        hostname: options.host
+    }, (res) =>{
+        res.on('data', data => {
+            console.log(data.ToString());
+        })
+    });
+}
+
 module.exports = {
-    GetToken
+    GetToken,
+    GetApiRequest,
+    DoRequest,
+    options
 }
